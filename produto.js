aa82a315324d4948f4c86e6edf2e1ffd55e2a49e
Kalven7 <kalven_moraes@estudante.sc.senai.br>
2022-08-04 22:19:35 -0300

    segunda aula de backend

---
+++ produto.js
@@ -0,0 +1,7 @@
+//modelo de produto
+
+//conexão banco de dados
+//servidor  = process.env
+// porta
+//user
+//pass 
--- sever.js
+++ sever.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import mysql from 'mysql';
 //LOAD CONFIG
 dotenv.config({path: './config/config.env'});
 
 //run server = express();
 const sever = express();
 const port = process.env.PORT || 7000;
-sever.listen(port, console.log("Servidor  processando na porta " + port + "..."));
+sever.listen(port, console.log("Servidor  processando em " + process.env.NODE_ENV  + " na porta " + port + "..."));
 
 sever.get('/', (req, resp) => {
  resp.send("God server only")
@@ -17,3 +17,23 @@ sever.get('/user', (req,resp) => {
     resp.send('Página do Usuário')
 
 });
+//MYSQL_HOST ='localhost'
+//MYSQL_port = ' 3306'
+//MYSQL_user = 'user'
+//MYSQL_password = ' pass' (to.produto)
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'foodreview',
+    password : 'foodreview',
+    database : 'foodreview'
+  });
+   
+  connection.connect();
+   
+  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+   
+  connection.end();
